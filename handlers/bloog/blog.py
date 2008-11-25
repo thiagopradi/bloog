thiagopradi/bloog

commit adda3e636b523b851ec7324b60bcc92254ad9516

Removed permalink property from articles, and changed to use the key name instead.
This allows us to use the more efficient gets instead of queries when simply fetching
articles by their URL.

handlers/bloog/blog.py
@@ -178,7 +178,7 @@ def process_article_edit(handler, permalink):
         if 'tags' in property_hash:
             property_hash['tag_keys'] = [get_tag_key(name) 
                                          for name in property_hash['tags']]
-        article = db.Query(models.blog.Article).filter('permalink =', permalink).get()
+        article = models.blog.Article.get_by_key_name('/' + permalink)
         before_tags = set(article.tag_keys)
         for key,value in property_hash.iteritems():
             setattr(article, key, value)
@@ -398,9 +398,7 @@ class ArticleHandler(restful.Controller):
                 return
 
         # Check undated pages
-        article = db.Query(models.blog.Article). \
-                     filter('permalink =', path).get()
-
+        article = models.blog.Article.get_by_key_name('/' + path)
         if not article:
             # This lets you map arbitrary URL patterns like /node/3
             #  to article properties, e.g. 3 -> legacy_id property
@@ -413,7 +411,7 @@ class ArticleHandler(restful.Controller):
 
     @restful.methods_via_query_allowed    
     def post(self, path):
-        article = db.Query(models.blog.Article).filter('permalink =', path).get()
+        article = models.blog.Article.get_by_key_name('/'+path)
         process_comment_submission(self, article)
 
     @authorized.role("admin")
@@ -458,8 +456,7 @@ class ArticleHandler(restful.Controller):
             query = models.blog.Tag.all()
             delete_entity(query)
         else:
-            article = db.Query(models.blog.Article). \
-                         filter('permalink =', path).get()
+            article = models.blog.Article.get_by_key_name('/'+path)
             for key in article.tag_keys:
                 db.get(key).counter.decrement()
             article.delete()
@@ -472,17 +469,15 @@ class BlogEntryHandler(restful.Controller):
         logging.debug("BlogEntryHandler#get for year %s, "
                       "month %s, and perm_link %s", 
                       year, month, perm_stem)
-        article = db.Query(models.blog.Article). \
-                     filter('permalink =', 
-                            year + '/' + month + '/' + perm_stem).get()
+        article = models.blog.Article.get_by_key_name('/%s/%s/%s' %
+                                                      (year, month, perm_stem))
         render_article(self, article)
 
     @restful.methods_via_query_allowed    
     def post(self, year, month, perm_stem):
         logging.debug("Adding comment for blog entry %s", self.request.path)
-        permalink = year + '/' + month + '/' + perm_stem
-        article = db.Query(models.blog.Article). \
-                     filter('permalink =', permalink).get()
+        article = models.blog.Article.get_by_key_name('/%s/%s/%s' %
+                                                      (year, month, perm_stem))
         if article:
             process_comment_submission(self, article)
         else:
@@ -491,16 +486,15 @@ class BlogEntryHandler(restful.Controller):
 
     @authorized.role("admin")
     def put(self, year, month, perm_stem):
-        permalink = year + '/' + month + '/' + perm_stem
+        permalink = '%s/%s/%s' % (year, month, perm_stem)
         logging.debug("BlogEntryHandler#put")
         process_article_edit(handler = self, permalink = permalink)
 
     @authorized.role("admin")
     def delete(self, year, month, perm_stem):
-        permalink = year + '/' + month + '/' + perm_stem
         logging.debug("Deleting blog entry %s", permalink)
-        article = db.Query(models.blog.Article). \
-                     filter('permalink =', permalink).get()
+        article = models.blog.Article.get_by_key_name('/%s/%s/%s' %
+                                                      (year, month, perm_stem))
         for key in article.tag_keys:
             db.get(key).counter.decrement()
         article.delete()

models/blog.py
@@ -29,25 +29,11 @@ import config
 import models
 from models import search
 
-# Handle generation of thread strings
-def get_thread_string(article, cur_thread_string):
-    min_str = cur_thread_string + '000'
-    max_str = cur_thread_string + '999'
-    q = db.GqlQuery("SELECT * FROM Comment " +
-                    "WHERE article = :1 " +
-                    "AND thread >= :2 AND thread <= :3",
-                    article, min_str, max_str)
-    num_comments = q.count(999)
-    if num_comments > 998:
-        return None         # Only allow 999 comments on each tree level
-    return cur_thread_string + "%03d" % (num_comments + 1)
-
 class Article(search.SearchableModel):
-    unsearchable_properties = ['permalink', 'legacy_id', 'article_type', 
+    unsearchable_properties = ['legacy_id', 'article_type', 
                                'excerpt', 'html', 'format', 'tag_keys']
     json_does_not_include = ['assoc_dict']
 
-    permalink = db.StringProperty(required=True)
     # Useful for aliasing of old urls
     legacy_id = db.StringProperty()
     title = db.StringProperty(required=True)
@@ -78,6 +64,12 @@ class Article(search.SearchableModel):
     # This lets us choose the proper javascript for pretty viewing.
     embedded_code = db.StringListProperty()
 
+    def __init__(self, permalink=None, **kwargs):
+        if permalink:
+            super(Article, self).__init__(key_name='/'+permalink, **kwargs)
+        else:
+            super(Article, self).__init__(**kwargs)
+
     def get_comments(self):
         """Return comments lexicographically sorted on thread string"""
         q = db.GqlQuery("SELECT * FROM Comment " +
@@ -98,6 +90,10 @@ class Article(search.SearchableModel):
     def get_associated_data(self):
         import pickle
         return pickle.loads(self.assoc_dict)
+
+    @property
+    def permalink(self):
+        return self.key().name()[1:]
 
     def full_permalink(self):
         return config.BLOG['root_url'] + '/' + self.permalink

commit 68d78627746fdbe5fb479ddd0d62c99ae819c057

Fixed a bug deleting posts introduced in commit adda3e63

handlers/bloog/blog.py
@@ -485,9 +485,9 @@ class BlogEntryHandler(restful.Controller):
 
     @authorized.role("admin")
     def delete(self, year, month, perm_stem):
+        permalink = '/%s/%s/%s' % (year, month, perm_stem)
         logging.debug("Deleting blog entry %s", permalink)
-        article = models.blog.Article.get_by_key_name('/%s/%s/%s' %
-                                                      (year, month, perm_stem))
+        article = models.blog.Article.get_by_key_name(permalink)
         for key in article.tags:
             models.blog.Tag.get_or_insert(key).counter.decrement()
         article.delete()