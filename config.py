f159c98616999346860eae1afe01700e74d35bcb
Thiago Pradi <tchandy@gmail.com>
2009-10-17 18:48:50 -0300

    Some configs

--- config.py
+++ config.py
@@ -15,13 +15,13 @@ BLOG = {
     "bloog_version": "0.8",
     "html_type": "text/html",
     "charset": "utf-8",
-    "title": "Bloog",
-    "author": "Bill Katz",
+    "title": "Ruby on Rails - Welcome Aboard",
+    "author": "Thiago Pradi",
     # This must be the email address of a registered administrator for the 
     # application due to mail api restrictions.
-    "email": "you@foo.com",
-    "description": "A RESTful Blog/Homepage for Google AppEngine.",
-    "root_url": "http://bloog.billkatz.com",
+    "email": "tchandy@gmail.com",
+    "description": "Some Scraps about Computer Science.",
+    "root_url": "http://thiagopradi.net",
     "master_atom_url": "/feeds/atom.xml",
     # By default, visitors can comment on article for this many days.
     # This can be overridden by setting article.allow_comments
@@ -59,48 +59,24 @@ PAGE = {
     "title": BLOG["title"],
     "articles_per_page": 5,
     "navlinks": [
-        { "title": "Articles", "description": "Bits of Info", 
+        { "title": "Artigos", "description": "Algumas Informações", 
           "url": "/articles"},
-        { "title": "Contact", "description": "Send me a note", 
+        { "title": "Contato", "description": "Me mande uma mensagem", 
           "url": "/contact"},
-    ],
-    "featuredMyPages": {
-        "title": "Bloog Development",
-        "description": "Get involved",
-        "entries": [
-            { "title": "Source Code", 
-              "url": "http://github.com/DocSavage/bloog", 
-              "description": "GitHub repository" },
-            { "title": "Tarball", 
-              "url": "http://github.com/DocSavage/bloog/tarball/master", 
-              "description": "Most recent snapshot" },
-            { "title": "Group", 
-              "url": "http://groups.google.com/group/bloog/topics", 
-              "description": "Developer discussion" },
-            { "title": "Author's Bloog", 
-              "url": "http://www.billkatz.com", 
-              "description": "What's brewing" },
-            { "title": "Architecture Diagram", 
-              "url": "/static/images/architecture2.png", 
-              "description": "RESTful Bloog" }
-        ]
-    },
+    ] },
     "featuredOthersPages": {
-        "title": "Google App Engine",
-        "description": "Developer Resources",
+        "title": "Desenvolvimento",
+        "description": "Alguns pedaços de código",
         "entries": [
-            { "title": "Google App Engine", 
-              "url": "http://code.google.com/appengine/", 
-              "description": "The mothership" },
-            { "title": "App Engine Group", 
-              "url": "http://groups.google.com/group/google-appengine", 
-              "description": "Developer group" },
-            { "title": "App Engine Open Source", 
-              "url": "http://groups.google.com/group/google-appengine/web/google-app-engine-open-source-projects", 
+            { "title": "Github", 
+              "url": "http://github.com/tchandy", 
               "description": "Code!" },
-            { "title": "App Engine Console", 
-              "url": "http://appengine.google.com", 
-              "description": "Your apps" }
+            { "title": "Working with rails", 
+              "url": "http://www.workingwithrails.com/person/12267-thiago-pradi", 
+              "description": "Me Recomende!" }
+						{ "title": "Twitter", 
+	             "url": "http://www.twitter.com/tchandy", 
+	             "description": "Alguns avisos!" }
         ]
     },
 }
